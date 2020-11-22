322c7b4abbf384450a304fd2b27c1d87327b0f18
Aleks <alekschernov79@gmail.com>
2020-11-23 00:27:44 +0200

    task8

---
+++ aleks-task8/main.js
@@ -0,0 +1,31 @@
+// Вариант без второй функции
+//function arrString() {
+//    var str = ["Я", "изучаю", "JavaScript", "прямо", "сейчас"];
+//    return(str);
+//}
+//var str = arrString();
+
+//var res = '';
+//for (var item of str) {
+//	res = `${res} ${item}`; //Вариант 1
+//res += item + ' '//Вариант 2
+//}
+//res = `${res}`;
+//console.log(res);
+//alert (res);
+
+function arrString() {
+    var str = ["Я", "изучаю", "JavaScript", "прямо", "сейчас"];
+    return(str);
+}
+var str = arrString();
+
+function newStr(str) {
+    var res = ' ';
+    for (var item of str) {
+        res += `${item}` ;
+    }
+    return res;
+}
+
+alert (newStr(str));
