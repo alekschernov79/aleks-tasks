38bca006fc885e19c6da6daaa6406027368c3339
Aleks <alekschernov79@gmail.com>
2020-11-30 00:18:37 +0200

    task9-10

--- aleks-task10/main.js
+++ aleks-task10/main.js
@@ -2,18 +2,32 @@ function whatsIt() {
     var str = 'BoMpWel';
     return str;
 }
-var str2 = whatsIt();
-function render(modelsStr) {
-	var res = "";
-	for (var char of str2) {
-var code = char.charCodeAt();
-if (code >= 65 && code <= 90 ) {
-res += (code + " ");
-}
-}
-var res2 = "";
 
-return res;
-}
- 
+var str2 = whatsIt();
+
+function render(modelsStr) {
+    var res = ""; // авто 1
+    var res2 = ''; // авто 2
+		for (var char of str2) {
+        var code = char.charCodeAt(); // поолучаешь код Юникода
+
+        if (code >= 65 && code <= 90 ) { // сравниваешь код буквы у условии
+            res += char; // не code а char(буквы), потому как ты же собираешь не код Юникода в слово, а слово из букв
+        } else { // если буква имеет код Юникода не попадающий в диапазон от 65 до 90, то выполнится условие else
+            res2 +=char; // собираешь второе название авто
+        }
+    }
+    // сталось вернуть не просто res, как в строке ниже, а обвернуть результаты в элементы списка и добавить в заранее созданный блок списка
+    
+ var res3 = [res, res2];
+ var userTable = document.getElementById("user-list");
+ var str3 = "";
+ for (var a in res3) {
+ 	str3 += `<li>${res3[a]}</li>`;
+ }
+  userTable.innerHTML = str3;
+
+ return str3;
+ }
+
 console.log(render());
--- aleks-task9/main.js
+++ aleks-task9/main.js
@@ -3,33 +3,65 @@ function arrNumber() {
         return arr;
     }
     var str = arrNumber();
-    function newStr(str) {
-    	var res = "";
-        for (var i = 0; i < 5; i++) {
-        
-    if (i % 1) {
-    		res += str[i] + " ";
-    	}
-    	else {
-    		res += str[i];
-    	}
-    }
-    return res;
-    }
-       var str2 = newStr(str);
+//    function newStr(str) {
+//    	var res = "";
+//        for (var i = 0; i < 5; i++) {
+//    if (i % 1) {
+//    		res += str[i] + " ";
+//    	}
+//    	else {
+//    		res += str[i];
+//    	}
+//    }
+//    return res;
+//    }
+//       var str2 = newStr(str);
+var str2 = str.join('');
     function newStr2(str2) {
     	var res2 = "";
         for (var i = 0; i < 15; i++) {
         
-    if (i % 2 == 0) {
+    if (str2[i] % 2 == 0) {
     		res2 += str2[i] + " ";
     	}
-    	else if (i % 2 == 1) {
+    	else  {
     		res2 += str2[i];
     	}
     }
     return res2;
     }
     var str3 = newStr2(str2);
-var str4 = str3.split(' ') 
-    alert(str4);
+    var str4 = str3.split(' ');
+    var str5 = str4.slice(0,-1); 
+    alert(str5);
+
+
+    function whatsIt() {
+        var str = 'BoMpWel';
+        return str;
+    }
+    var str2 = whatsIt();
+    function render(modelsStr) {
+        var userTable = document.getElementById("user-list");
+        var res = "";
+        var res2 = "";
+        var res3 = ['res', 'res2'];
+        for (var char of str2) {
+    var code = char.charCodeAt();
+    if (code >= 65 && code <= 90 ) {
+    res += char;
+    }
+    else {
+        res2 += char;
+    }
+    }
+    //return res3 = [res, res2];
+    var res4 = "";
+    for (var k in res3) {
+        res4 += `<li>${res3[k]}</li>`;
+    }
+    console.log(res4);
+    userTable.innerHTML = res4;
+    }
+     
+    console.log(render());
